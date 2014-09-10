5635ce78fded846a19a8ab7152e06ac708cee1f2
DwellableData <katie@dwellable.com>
2014-09-10 12:25:49 -0700

    update labels

--- purchase-to-profit/statehover.js
+++ purchase-to-profit/statehover.js
@@ -78,11 +78,13 @@
 		d3.select(id).selectAll(".label")
 			.data(uStatePaths).enter().append("text").attr("class","label").attr("d",function(d){ return d.d;})
 				.text(function(d){
+					console.log(d[id]);
+					console.log(key_stateids.indexOf(d[id]));					
 					if (key_stateids.indexOf(d[id]) > -1) {
 						console.log(data[d.id].label)
 			        	return data[d.id].label;
-					}
-
+					};
+					return "";
 			    })
 			    .attr("x", function(d){
 			        return data[d.id].longitude;
