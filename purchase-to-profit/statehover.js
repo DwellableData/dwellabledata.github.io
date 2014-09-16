06d5e77a152fb0d3f70c4b21834f47b024759440
DwellableData <katie@dwellable.com>
2014-09-16 11:17:33 -0700

    update to light grey color

--- purchase-to-profit/statehover.js
+++ purchase-to-profit/statehover.js
@@ -79,7 +79,7 @@
 				        	return statedata[statename].latitude;					
 						}
 				    })
-				    .attr("fill", rgb(116, 130, 138));
+				    .attr("fill", "#D3D3D3");
 
 
 			d3.select(id).selectAll(".labelblack")
